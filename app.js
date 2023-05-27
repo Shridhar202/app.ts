32267694eb5e379511ce4c8a84471ec27599621d
Shridhar202 <shridhargawali2002@gmail.com>
2023-05-27 16:36:29 +0530

    main: substraction,multiplication,divide

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 console.log("this is a ts file");
 console.log("this is a ts file2");
 var addFun = function (a, b) { return a + b; };
-var addFunReturn = addFun(1, 2)
-console.log(addFunReturn);
+var addFunReturn = addFun(1, 2);
+console.log('addFunReturn', addFunReturn);
+var subFun = function (a, b) { return a - b; };
+var subFunReturn = subFun(10, 5);
+console.log('subFunReturn', subFunReturn);
+var multiFun = function (a, b) { return a * b; };
+var multiFunReturn = multiFun(10, 5);
+console.log('multiFunReturn', multiFunReturn);
+var divFun = function (a, b) { return a / b; };
+var divFunReturn = divFun(10, 5);
+console.log('divFunReturn', divFunReturn);
--- app.ts
+++ app.ts
@@ -3,4 +3,16 @@ console.log("this is a ts file2")
 
 const addFun = (a:number,b:number) => a+b;
 const addFunReturn = addFun(1,2);
-console.log(addFunReturn);
+console.log('addFunReturn',addFunReturn);
+
+const subFun = (a:number, b:number) => a-b;
+const subFunReturn = subFun(10,5);
+console.log('subFunReturn',subFunReturn);
+
+const multiFun = (a:number, b:number) => a*b;
+const multiFunReturn = multiFun(10,5);
+console.log('multiFunReturn',multiFunReturn);
+
+const divFun = (a:number, b:number) => a/b;
+const divFunReturn = divFun(10,5);
+console.log('divFunReturn',divFunReturn);
