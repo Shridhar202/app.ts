a35eb5eea5ab7d5be311a9c5403a76c5c7f437a3
Shridhar202 <shridhargawali2002@gmail.com>
2023-05-27 16:33:30 +0530

    main: substraction,multiplication,divide

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("this is a ts file");
+console.log("this is a ts file2");
+var addFun = function (a, b) { return a + b; };
+var addFunReturn = addFun(1, 2)
+console.log(addFunReturn);
---
+++ app.ts
@@ -0,0 +1,6 @@
+console.log("this is a ts file")
+console.log("this is a ts file2")
+
+const addFun = (a:number,b:number) => a+b;
+const addFunReturn = addFun(1,2);
+console.log(addFunReturn);
